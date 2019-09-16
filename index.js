4ec817817ff2cebf14a1edb108d0169d8981900c
yoyo <jihu777@gmail.com>
2019-09-16 09:34:18 +0800

    change api

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const _ = require("lodash");
+const _ = require('lodash');
 
 module.exports = (api, opts) => {
-  api.modifyDefaultConfig(memo => {
+  api.modifyAFWebpackOpts(memo => {
     return {
       ...memo,
       urlLoaderExcludes: [/\.svg$/],
-      chainWebpack(config) {
-        config.module
-          .rule("js")
-          .use("babel-loader")
-          .tap(opts => {
-            _.remove(opts.plugins, plugin => /babel-plugin-named-asset-import/.test(plugin[0]));
-            return opts;
-          });
-
-        config.module
-          .rule("custom-svgr")
-          .test(/\.svg$/)
-          .use("babel")
-          .loader(require.resolve("babel-loader"))
-          .end()
-          .use("svgr")
-          .loader(require.resolve("@svgr/webpack"))
-          .options({ ref: true, babel: true })
-          .end()
-          .use("url-loader")
-          .loader(require.resolve("url-loader"))
-          .end();
-      }
     };
+  });
+  api.chainWebpackConfig(config => {
+    config.module
+      .rule('js')
+      .use('babel-loader')
+      .tap(opts => {
+        _.remove(opts.plugins, plugin => /babel-plugin-named-asset-import/.test(plugin[0]));
+        return opts;
+      });
+    config.module
+      .rule('custom-svgr')
+      .test(/\.svg$/)
+      .use('babel')
+      .loader(require.resolve('babel-loader'))
+      .end()
+      .use('svgr')
+      .loader(require.resolve('@svgr/webpack'))
+      .options({ ref: true, babel: true })
+      .end()
+      .use('url-loader')
+      .loader(require.resolve('url-loader'))
+      .end();
+    return config;
   });
 };
